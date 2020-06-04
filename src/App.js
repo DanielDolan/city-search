2bb3fff3aa46331b84f653ddc7e3c20465b457da
Daniel Dolan <danieldolan7@gmail.com>
2020-06-04 17:58:53 -0400

    addition of styling

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>ReactJS with APIs</h1>
-        <City City="DALLAS" />
+        <h1>Turn City Name into zips</h1>
+        <City> </City>
       </div>
     );
   }
--- src/components/city.js
+++ src/components/city.js
@@ -1,43 +1,46 @@
 import React, { Component } from "react";
+import "./city.css"
 import axios from "axios";
-
 class City extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { city: null };
-  }
-
-  componentDidMount() {
-    axios
-      .get("http://ctp-zip-api.herokuapp.com/city/" + this.props.City)
-      .then((response) => {
-        const data = response.data;
-
-        const newCityObj = {
-          city: data.City,
-        };
-
-        this.setState({ city:newCityObj });
-      })
-      .catch((err) => console.log(err));
-  }
-
-  render() {
-    let display;
-    if (!this.state.city) {
-      display = <p>Loading...</p>;
-    } else {
-      display = (
-        <>
-          <ul>
-            <li>{this.state.city.Zipcode} </li>
-          </ul>
-        </>
-      );
+    constructor(props) {
+        super(props);
+        this.state = { 
+            city: "TEXAS",
+            records: [] ,
+         }
     }
+    componentDidMount() {
+        axios 
+        .get("http://ctp-zip-api.herokuapp.com/city/"+this.state.city.toUpperCase())
+        .then((response) => {
+            
+            this.setState({records:response.data});
+        })
+        .catch((err) => console.log(err));
+    }
+    render() {
+        return (
+            <>
+                <div>
+                    <h2> Please enter City Name:</h2>
+                    <input 
+                    className= "prompt-line"
+                    type="text"
+                    onChange= {(event) => this.setState({city: event.target.value})}
+                    placeholder={this.state.city}
+                    /> 
+                </div>
 
-    return <div className="city">{display}</div>;
-  }
-}
-
-export default City;
+                <button 
+                ClassName= "button-container" 
+                onClick={() => {
+                    this.componentDidMount();
+                }}
+                >
+                FIND 
+                </button>
+                </>
+        );
+            }
+        }
+        export default City;
